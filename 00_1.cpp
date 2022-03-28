cd87888475a87c4ef47d3994ed80b72129737315
RinaLing <102519908+RinaLing@users.noreply.github.com>
2022-03-28 13:32:28 +0300

    Add files via upload

---
+++ 00_1.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 13;
+    int y = 73;
+
+    std::cout << " x = " << x << "\n";
+    std::cout << " y = " << y << "\n";
+    std::cout << " x + y = " << x + y << "\n";
+    std::cout << " x - y = " << x - y << "\n";
+    std::cout << " x / y = " << x / y << "\n";
+    std::cout << " x % y = " << x % y << "\n";
+
+    return 0;
+}
